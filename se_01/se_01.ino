7b0b90ff673413960327251ef88155e72cc7ab17
Marcio <marciomds94@hotmail.com>
2019-10-01 18:03:19 +0100

    Final
    Tudo a funcionar ldr com relay

--- se_01/se_01.ino
+++ se_01/se_01.ino
@@ -1,24 +1,25 @@
 //VARIAVEIS
-  int relayPIN = 8;
+  int relayPIN = 7;
   int ldrPIN = 1;
   int ldrValue = 0;
-  int Treshold = 500;
+  int Treshold = 100;
   
 void setup() {
   pinMode(ldrPIN, OUTPUT); // LDR PIN
   pinMode(relayPIN, OUTPUT); // RELAY PIN
+//  digitalWrite(relayPIN, HIGH);
   Serial.begin(9600);
 }
 
 void loop() {
-  ldrValue = analogRead(ldrPIN); //(0-1023)
+  ldrValue = analogRead(ldrPIN); //(10-617)
 
   if (ldrValue >= Treshold){
-    digitalWrite(relayPIN, HIGH);
-  } else {
     digitalWrite(relayPIN, LOW);
+  } else {
+    digitalWrite(relayPIN, HIGH);
   }
 
-  Serial.println(ldrValue);
+//  Serial.println(ldrValue);
   delay(100);
 }
